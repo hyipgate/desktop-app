a2d0630ce4cfe8ca414b3f684e64d5967c8051a4
arrietaeguren <arrietaeguren@gmail.com>
2017-05-18 23:50:24 +0100

    Added scene list on film view

--- app/controllers/filmController.js
+++ app/controllers/filmController.js
@@ -1,6 +1,6 @@
 angular.module('filmCtrl', ['ngMaterial'])
 
-.controller('FilmController', function($rootScope, $scope, service, $location, $mdBottomSheet) {
+.controller('FilmController', function($rootScope, $scope, service, $location, $mdBottomSheet, $mdDialog) {
   var vm = this;
   
   vm.getMovie = function(){
@@ -29,13 +29,47 @@ angular.module('filmCtrl', ['ngMaterial'])
     $mdBottomSheet.hide(clickedItem);
   };
 
-  vm.get_available_players = function(){
-    $rootScope.utils.get_available_players().then(function(data){
-      console.log(data);
-      vm.players = data;
-    });
+
+  vm.players = ["Netflix","Amazon Video"] //TODO: this should be in the movie data
+
+
+
+  $scope.editScene = function(id,$event) {
+    $mdDialog.show({
+      targetEvent: $event,
+      template:
+        '<md-dialog>' +
+        ' <md-dialog-content>'+
+        '<label>Start:</label><input type="text" ng-model="scene.start" placeholder="ms"/><br>'+
+        '<label>End:</label><input type="text" ng-model="scene.end" placeholder="ms"/><br>'+
+        '<label>Comment:</label><input type="text" ng-model="scene.comment" placeholder="Brief comment"/><br>'+
+        '</md-dialog-content>' +
+        '  <md-dialog-actions>' +
+        '    <md-button>Preview</md-button>' +
+        '    <md-button ng-click="closeDialog()" class="md-primary">Finish</md-button>' +
+        '  </md-dialog-actions>' +
+        '</md-dialog>',
+      locals: { id: id },
+      controller: ['$scope', 'id', function($scope, id) {
+        $scope.scene = vm.movieData.scenes[id];
+        $scope.closeDialog = function() {
+          $mdDialog.hide();
+        }
+      }]
+    })
   }
 
-  vm.get_available_players();
-
-});
+}).filter('minutes', function() {
+  return function( input ) {
+    input = input || 0;
+    return Math.floor(input/60)
+  };
+}).filter('time', function() {
+  return function(start, end) {
+    return Math.floor(end-start)
+  };
+}).filter('tags', function() {
+  return function( input ) {
+    return input.join(", ")
+  };
+})
